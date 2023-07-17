db83c29e98cab54b89c878c4656d9515013fbb1f
jcampbell57 <jcampbell57@gmail.com>
2023-07-17 11:11:30 -0700

    initial methods added to v2

--- version_2/game.rb
+++ version_2/game.rb
@@ -1,33 +1,58 @@
 # frozen_string_literal: true
 
-# methods to initialize game
+# primary game logic
 class Game
+  attr_accessor :mode, :code
+
   def initialize
-    self.mode = 1
     display_info
+    # prompt_game_mode
+    self.mode = 1
+    start_game
   end
+
+  # methods to initialize game
 
   def display_info
     puts 'Lets play Mastermind in the console!'
+    puts 'Game Info:'
+    puts '- The code breaker has 12 guesses to break the code.'
+    puts '- Each code has four values.'
+    puts "- Each value could be any number from '1' to '6'"
     puts 'Hints:'
     puts "- 'X' represents a correct value in the correct position."
     puts "- 'O' represents a correct value in the incorrect position."
-    puts '- Each code has four values.'
-    puts "- Each value could be any number from '1' to '6'"
   end
 
   def prompt_game_mode
-    puts 'Please select game mode: '
-    puts "Input '1' for code breaker."
-    puts "Input '2' for code setter."
-    print 'Game mode selection: '
+    print "Input '1' for code breaker mode or '2' for code setter mode: "
     self.mode = verify_mode_selection(gets.chomp.to_i)
   end
 
   def verify_mode_selection(integer)
     return integer if integer.between?(1, 2)
 
-    puts "Game mode input must be '1' or '2'"
+    print "Game mode input must be '1' or '2': "
     verify_mode_selection(gets.chomp.to_i)
   end
+
+  def start_game
+    mode == 1 ? code_breaker_mode : code_setter_mode
+  end
+
+  # code breaker
+
+  def code_breaker_mode
+    randomize_code
+  end
+
+  def randomize_code
+    code_bank %w[1 2 3 4 5 6]
+    self.code = []
+    4.times { code << code_bank.sample }
+  end
+
+  # code setter
+
+  def code_setter_mode; end
 end
